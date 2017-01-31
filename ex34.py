269088979ef3b61e2c2de60baca4450b1d9d058e
camaya714 <camaya714@gmail.com>
2017-01-31 01:06:57 -0500

    Add existing file

---
+++ ex34.py
@@ -0,0 +1,43 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"The first (1st) animal is at 0 and is a bear."
+"The second (2nd) animal is at 1 and is a python."
+"The third (3rd) animal is at 2 and is a peacock."
+"The fourth (4th) animal is at 3 and is a kangaroo."
+"The fifth (5th) animal is at 4 and is a whale."
+"The sixth (6th) animal is at 5 and is a platypus."
+
+print animals[0]
+print animals[1]
+print animals[2]
+print animals[3]
+print animals[4]
+print animals[5]
+
+checklist = ['ticket', 'airfare', 'campdues', 'buspass', 'supplies']
+
+"The first item is 0 and is the ticket."
+"The third item is 2 and are the campdues."
+"The fifth item is 4 and are the supplies."
+"The second item is 1 and is the airfare."
+"The fourth item is 3 and is the buspass."
+
+print checklist[0]
+print checklist[1]
+print checklist[2]
+print checklist[3]
+print checklist[4]
+
+skills = ['python', 'R', 'SQL', 'Hadoop', 'Spark']
+
+"The first skill is 0 and is python."
+"The second skill is 1 and is R."
+"The third skill is 2 and is SQL."
+"The fourth skill is 3 and is Hadoop."
+"The fifth skill is 4 and is Spark."
+
+print skills[0]
+print skills[4]
+print skills[1]
+print skills[3]
+print skills[2]
